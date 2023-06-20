64c115a5ca7ee8f0a07a909d321d7497242bbd72
symbatzinZinatova <symbat.zinat29@gmail.com>
2023-06-20 19:58:02 +0600

    number revert corrected

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -5,11 +5,10 @@ public class NumberReverter {
     public void revert(int number) {
         int result = 0;
         int remainder;
-
         while (number > 0) {
             remainder = number % 10;
             result = (result * 10) + remainder;
-
+            number = number / 10;
         }
         System.out.println(result);
     }
